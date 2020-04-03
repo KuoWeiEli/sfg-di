385971d3ee1f8d086d041fc10f41bd29483a6f73
eli.huang <naughtyowlkid@gmail.com>
2020-04-04 07:16:34 +0800

    Multi-propeties demo

--- src/main/java/guru/springframework/sfg/di/config/PropertyConfig.java
+++ src/main/java/guru/springframework/sfg/di/config/PropertyConfig.java
@@ -1,15 +1,21 @@
 package guru.springframework.sfg.di.config;
 
 import guru.springframework.sfg.di.examplebean.FakeDataSource;
+import guru.springframework.sfg.di.examplebean.FakeJmsBroker;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 
 @Configuration
-@PropertySource("classpath:datasource.properties")
+//@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
+// 多參數時可以使用可讀性較高的方式：
+@PropertySources({
+        @PropertySource("classpath:datasource.properties"),
+        @PropertySource("classpath:jms.properties")})
 public class PropertyConfig {
     private final Environment env;
     @Value("${guru.username}")
@@ -18,6 +24,13 @@ public class PropertyConfig {
     private String password;
     @Value("${guru.url}")
     private String url;
+
+    @Value("${guru.jms.username}")
+    private String jmsUser;
+    @Value("${guru.jms.password}")
+    private String jmsPassword;
+    @Value("${guru.jms.url}")
+    private String jmsUrl;
 
     public PropertyConfig(Environment env) {
         this.env = env;
@@ -32,6 +45,15 @@ public class PropertyConfig {
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
+    }
+
+    @Bean
+    public FakeJmsBroker fakeJmsBroker() {
+        FakeJmsBroker fakeJmsBroker = new FakeJmsBroker();
+        fakeJmsBroker.setUser(jmsUser);
+        fakeJmsBroker.setPassword(jmsPassword);
+        fakeJmsBroker.setUrl(jmsUrl);
+        return fakeJmsBroker;
     }
 
     @Bean
